a231d27e7464fb6d2c37577a7334fb213a35a08a
Caio-C8 <caiomgf50@gmail.com>
2026-04-03 15:43:34 -0300

    feat: mapeia modulo de autenticacao e adiciona a verificacao dos guards nas rotas

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,9 +3,12 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PersistenceModule } from "./common/prisma/prisma.module";
-import { APP_FILTER, APP_INTERCEPTOR } from "@nestjs/core";
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
 import { RespostaInterceptor } from "./common/interceptors/resposta.interceptor";
 import { ExcecaoHttp } from "./common/filters/excecao-http.filter";
+import { JwtGuard } from "./common/guards/jwt.guard";
+import { AutenticacaoModule } from "./modules/autenticacao/autenticacao.module";
+import { PapeisGuard } from "./common/guards/papeis.guard";
 
 @Module({
   imports: [
@@ -13,12 +16,15 @@ import { ExcecaoHttp } from "./common/filters/excecao-http.filter";
       isGlobal: true,
     }),
     PersistenceModule,
+    AutenticacaoModule,
   ],
   controllers: [AppController],
   providers: [
     AppService,
     { provide: APP_INTERCEPTOR, useClass: RespostaInterceptor },
     { provide: APP_FILTER, useClass: ExcecaoHttp },
+    { provide: APP_GUARD, useClass: JwtGuard },
+    { provide: APP_GUARD, useClass: PapeisGuard },
   ],
 })
 export class AppModule {}

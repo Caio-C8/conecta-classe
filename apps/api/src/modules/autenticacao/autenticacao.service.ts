d7e458f0850f5ab50ff9a1b00541bf4f734e119f
Caio-C8 <caiomgf50@gmail.com>
2026-04-04 14:40:53 -0300

    feat: cria rota para trocar senha e altera rota de login para retornar o novo atributo

--- apps/api/src/modules/autenticacao/autenticacao.service.ts
+++ apps/api/src/modules/autenticacao/autenticacao.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { AutenticacaoRepository } from "./autenticacao.repository";
-import { LoginInput, RespostaLogin } from "@repo/types";
+import { LoginInput, RespostaLogin, TrocarSenhaInput } from "@repo/types";
 import * as bcrypt from "bcrypt";
 import { JwtService } from "@nestjs/jwt";
 
@@ -12,7 +12,8 @@ export class AutenticacaoService {
   ) {}
 
   async login(dados: LoginInput): Promise<RespostaLogin> {
-    const usuario = await this.autenticacaoRepository.getUsuario(dados);
+    const usuario =
+      await this.autenticacaoRepository.getUsuarioPorUsuario(dados);
 
     if (!usuario) {
       throw new UnauthorizedException("Usuário ou senha incorretos.");
@@ -28,6 +29,7 @@ export class AutenticacaoService {
       sub: usuario.id,
       usuario: usuario.usuario,
       papel: usuario.papel,
+      trocar_senha: usuario.trocar_senha,
     };
 
     return {
@@ -36,6 +38,50 @@ export class AutenticacaoService {
         id: usuario.id,
         nome: usuario.nome,
         papel: usuario.papel,
+        trocar_senha: usuario.trocar_senha,
+      },
+    };
+  }
+
+  async trocarSenha(
+    id: number,
+    dados: TrocarSenhaInput,
+  ): Promise<RespostaLogin> {
+    if (dados.nova_senha !== dados.confirmar_senha) {
+      throw new UnauthorizedException("As senhas não coincidem.");
+    }
+
+    const usuario = await this.autenticacaoRepository.getUsuarioPorId(id);
+
+    if (!usuario) {
+      throw new UnauthorizedException("Usuário não encontrado.");
+    }
+
+    if (!usuario.trocar_senha) {
+      throw new UnauthorizedException(
+        "Você não tem permissão para alterar a senha.",
+      );
+    }
+
+    dados.nova_senha = await bcrypt.hash(dados.nova_senha, 10);
+
+    const usuarioAtualizado =
+      await this.autenticacaoRepository.updateSenhaUsuario(id, dados);
+
+    const payload = {
+      sub: usuarioAtualizado.id,
+      usuario: usuarioAtualizado.usuario,
+      papel: usuarioAtualizado.papel,
+      trocar_senha: usuarioAtualizado.trocar_senha,
+    };
+
+    return {
+      token: await this.jwtService.signAsync(payload),
+      usuario: {
+        id: usuarioAtualizado.id,
+        nome: usuarioAtualizado.nome,
+        papel: usuarioAtualizado.papel,
+        trocar_senha: usuarioAtualizado.trocar_senha,
       },
     };
   }

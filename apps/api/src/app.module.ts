809fa5d81cf128a0ee689607a459953b036e2f7c
Caio-C8 <caiomgf50@gmail.com>
2026-04-04 14:37:27 -0300

    feat: adiciona guard de troca de senha e adiciona validacao dos schemas zod

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { PersistenceModule } from "./common/prisma/prisma.module";
-import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from "@nestjs/core";
 import { RespostaInterceptor } from "./common/interceptors/resposta.interceptor";
 import { ExcecaoHttp } from "./common/filters/excecao-http.filter";
 import { JwtGuard } from "./common/guards/jwt.guard";
 import { AutenticacaoModule } from "./modules/autenticacao/autenticacao.module";
 import { PapeisGuard } from "./common/guards/papeis.guard";
+import { ForcarTrocaSenhaGuard } from "./common/guards/forcar-troca-senha.guard";
+import { ZodValidationPipe } from "nestjs-zod";
 
 @Module({
   imports: [
@@ -23,7 +25,9 @@ import { PapeisGuard } from "./common/guards/papeis.guard";
     AppService,
     { provide: APP_INTERCEPTOR, useClass: RespostaInterceptor },
     { provide: APP_FILTER, useClass: ExcecaoHttp },
+    { provide: APP_PIPE, useClass: ZodValidationPipe },
     { provide: APP_GUARD, useClass: JwtGuard },
+    { provide: APP_GUARD, useClass: ForcarTrocaSenhaGuard },
     { provide: APP_GUARD, useClass: PapeisGuard },
   ],
 })

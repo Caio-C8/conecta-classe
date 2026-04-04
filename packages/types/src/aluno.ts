b386b957e2f6d83e50bf439ac3cbf524f5e72d46
Caio-C8 <caiomgf50@gmail.com>
2026-04-04 14:35:05 -0300

    feat: tira atributos que foram removidos do banco

--- packages/types/src/aluno.ts
+++ packages/types/src/aluno.ts
@@ -3,8 +3,5 @@ import { Usuario } from "./usuario";
 export interface Aluno {
   id: number;
   usuario_id: number;
-  deleted_at: Date | null;
-  created_at: Date;
-  updated_at: Date;
   usuario: Usuario;
 }

64fdd13cc53dced8881b5a729f6659b5cac28ee8
Caio-C8 <caiomgf50@gmail.com>
2026-05-12 13:44:20 -0300

    feat: cria schema para criar disciplina e adiciona nome_search em disciplina

--- packages/types/src/disciplina.ts
+++ packages/types/src/disciplina.ts
@@ -2,10 +2,12 @@ import { ProfessorTurma } from "./professor-turma";
 import { Aula } from "./aula";
 import { Evento } from "./evento";
 import { RendimentoDisciplina } from "./rendimento-disciplina";
+import z from "zod";
 
 export interface Disciplina {
   id: number;
   nome: string;
+  nome_search: string;
   deleted_at: Date | null;
   created_at: Date;
   updated_at: Date;
@@ -15,3 +17,12 @@ export interface Disciplina {
   eventos?: Evento[];
   rendimentos?: RendimentoDisciplina[];
 }
+
+export const CreateDisciplinaSchema = z.object({
+  nome: z.string({
+    required_error: "Preencha o campo nome.",
+    invalid_type_error: "Nome inválido.",
+  }),
+});
+
+export type CreateDisciplinaInput = z.infer<typeof CreateDisciplinaSchema>;

d0a5db627013545b3bd4f6f96c39b3838901a720
Caio-C8 <caiomgf50@gmail.com>
2026-05-12 13:44:47 -0300

    feat: cria endpoint para criar uma disciplina

--- apps/api/src/modules/disciplina/disciplina.module.ts
+++ apps/api/src/modules/disciplina/disciplina.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { DisciplinaService } from "./disciplina.service";
 import { DisciplinaRepository } from "./disciplina.repository";
+import { DisciplinaController } from "./disciplina.controller";
 
 @Module({
   imports: [],
   providers: [DisciplinaService, DisciplinaRepository],
-  controllers: [],
+  controllers: [DisciplinaController],
   exports: [DisciplinaService, DisciplinaRepository],
 })
 export class DisciplinaModule {}
--- apps/api/src/modules/disciplina/disciplina.service.ts
+++ apps/api/src/modules/disciplina/disciplina.service.ts
@@ -1,10 +1,22 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { DisciplinaRepository } from "./disciplina.repository";
-import { Disciplina } from "@repo/types";
+import { CreateDisciplinaInput, Disciplina } from "@repo/types";
 
 @Injectable()
 export class DisciplinaService {
   constructor(private readonly disciplinaRepository: DisciplinaRepository) {}
+
+  async createDisciplina(dados: CreateDisciplinaInput): Promise<Disciplina> {
+    const disciplina = await this.disciplinaRepository.findDisciplinaPorNome(
+      dados.nome,
+    );
+
+    if (disciplina) {
+      throw new BadRequestException("Já existe uma disciplina com este nome.");
+    }
+
+    return await this.disciplinaRepository.createDisciplina(dados);
+  }
 
   async getDisciplinasPorTurmas(turmaId: number): Promise<Disciplina[]> {
     return await this.disciplinaRepository.findDisciplinasPorTurma(turmaId);

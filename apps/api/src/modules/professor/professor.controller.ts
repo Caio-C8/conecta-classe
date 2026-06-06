42027d8af78df831dd314811065ca0a16518ff88
Caio César Alves de Freitas <168493688+Caio-C8@users.noreply.github.com>
2026-06-06 15:38:07 -0300

    feat: integra parte da tela home do professor com a api (#81)

--- apps/api/src/modules/professor/professor.controller.ts
+++ apps/api/src/modules/professor/professor.controller.ts
@@ -1,58 +1,56 @@
-import { Controller, Get, UseGuards,Param, Query } from '@nestjs/common';
-import { ProfessorService } from './professor.service';
-import { JwtGuard } from '../../common/guards/jwt.guard';
-import { PapeisGuard } from '../../common/guards/papeis.guard';
-import { Papeis } from '../../common/decorators/papeis.decorator';
-import { GetUsuario } from '../../common/decorators/get-usuario.decorator';
-import { Papel } from '@repo/types';
-import { Body, Post } from '@nestjs/common'; 
-import { CreateEventoDto } from './dto/create-evento.dto';
+import { Controller, Get, UseGuards, Param, Query } from "@nestjs/common";
+import { ProfessorService } from "./professor.service";
+import { JwtGuard } from "../../common/guards/jwt.guard";
+import { PapeisGuard } from "../../common/guards/papeis.guard";
+import { Papeis } from "../../common/decorators/papeis.decorator";
+import { GetUsuario } from "../../common/decorators/get-usuario.decorator";
+import { Papel } from "@repo/types";
+import { Body, Post } from "@nestjs/common";
+import { CreateEventoDto } from "./dto/create-evento.dto";
 
-
-@Controller('professor')
+@Controller("professor")
 @UseGuards(JwtGuard, PapeisGuard)
-@Papeis(Papel.PROFESSOR) 
+@Papeis(Papel.PROFESSOR)
 export class ProfessorController {
   constructor(private readonly professorService: ProfessorService) {}
 
-  @Get('turmas')
-  async getTurmas(@GetUsuario('id') usuarioId: number) {
+  @Get("turmas")
+  async getTurmas(@GetUsuario("id") usuarioId: number) {
     return this.professorService.buscarTurmasDoProfessor(usuarioId);
   }
 
-  @Post('eventos')
+  @Post("eventos")
   async criarEvento(
-    @GetUsuario('id') usuarioId: number,
-    @Body() dto: CreateEventoDto, 
+    @GetUsuario("id") usuarioId: number,
+    @Body() dto: CreateEventoDto,
   ) {
     return this.professorService.criarEvento(usuarioId, dto);
   }
 
-  @Get('eventos/proximos')
-    async getProximosEventos(@GetUsuario('id') usuarioId: number) {
-      return this.professorService.buscarProximosEventos(usuarioId);
-    }
+  @Get("eventos/proximos")
+  async getProximosEventos(@GetUsuario("id") usuarioId: number) {
+    return this.professorService.buscarProximosEventos(usuarioId);
+  }
 
-
-  @Get('eventos/:id/notas')
+  @Get("eventos/:id/notas")
   async getDiarioDeNotas(
-    @GetUsuario('id') usuarioId: number,
-    @Param('id') eventoId: string 
+    @GetUsuario("id") usuarioId: number,
+    @Param("id") eventoId: string,
   ) {
     return this.professorService.buscarDiarioDeNotas(usuarioId, +eventoId);
   }
 
-  
-  @Get('turmas/:id/alunos')
+  @Get("turmas/:id/alunos")
   async getAlunosDaTurma(
-  @Param('id') turmaId: string,
-  @Query('aula_id') aula_idStr?: string 
+    @Param("id") turmaId: string,
+    @Query("aula_id") aula_idStr?: string,
   ) {
-    let numeroAulaId = aula_idStr?  Number(aula_idStr) : undefined
-    let numeroTurmaId = Number(turmaId)
+    let numeroAulaId = aula_idStr ? Number(aula_idStr) : undefined;
+    let numeroTurmaId = Number(turmaId);
 
-    return this.professorService.buscarAlunosParaChamada(numeroTurmaId,numeroAulaId)
+    return this.professorService.buscarAlunosParaChamada(
+      numeroTurmaId,
+      numeroAulaId,
+    );
   }
-
-
 }
